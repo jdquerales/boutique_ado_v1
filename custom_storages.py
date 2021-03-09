9028592faa3ee91a67677ed3fc4191271aaa78e4
jdquerales <jquerales72@gmail.com>
2021-03-09 16:09:27 +0000

    AWS S3 add to deployment to Heroku

---
+++ custom_storages.py
@@ -0,0 +1,8 @@
+import django.conf import settings
+from storages.backends.s3boto3 import S3Boto2Storage
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
