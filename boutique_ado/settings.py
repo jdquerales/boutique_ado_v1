588936d881c09a297ef46d2e968eb153e7f0d4c0
jdquerales <jquerales72@gmail.com>
2021-03-09 16:27:22 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -186,6 +186,12 @@ FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'jose-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-1'
