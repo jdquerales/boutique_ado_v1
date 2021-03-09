43ba6fce1dbc2f3743933411ee4592dfd57a6afe
jdquerales <jquerales72@gmail.com>
2021-03-09 14:50:29 +0000

    Deployment to Heroku part II

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'i4ju2xn5ay!x3g%$^&a-s$usdfz(l%ld*25er%=_#1w$2zzwl0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jose-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,14 +124,16 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
-
 
 
 # Password validation
