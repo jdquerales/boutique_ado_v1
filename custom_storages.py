74f14dd57bf01fcf5f7ea87226cde38d6a705c2a
jdquerales <jquerales72@gmail.com>
2021-03-09 16:18:37 +0000

    Fix custom_storages.py file

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,5 @@
 import django.conf import settings
-from storages.backends.s3boto3 import S3Boto2Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
