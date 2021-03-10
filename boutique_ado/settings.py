0e4a44bda18438e07ae38c0b474e132dd4a77c9c
jdquerales <jquerales72@gmail.com>
2021-03-10 10:57:38 +0000

    Fix port for email in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -229,7 +229,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 567
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
